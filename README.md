ce89af32b2e58b3898c789fa7ebebd04011a1d47
iamlucasmateo <perrone.lucasmateo@gmail.com>
2022-11-02 19:08:38 -0300

    some details

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # fcc-farm
 FARM app
+
+# Commands
+## Start DB
+https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/
+## Start backend
+uvicorn backend.main:app --port 2020
+## Start frontend
+npm start
--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,14 +1,16 @@
+const BACKEND_URL = "http://localhost:2020/api/todo/"
+
 export const getTodos = async () => {
-    const todos = await fetch("http://localhost:8000/api/todo/");
+    const todos = await fetch(BACKEND_URL);
     return todos.json()
 }
 
 export const deleteTodo = async (itemTitle) => {
-    await fetch(`http://localhost:8000/api/todo/${itemTitle}`, { method: "DELETE" })
+    await fetch(`${BACKEND_URL}${itemTitle}`, { method: "DELETE" })
 }
 
 export const postTodo = async (title, description) => {
-    await fetch("http://localhost:8000/api/todo", {
+    await fetch(BACKEND_URL, {
         method: "POST",
         body: JSON.stringify({ title, description })
     })
